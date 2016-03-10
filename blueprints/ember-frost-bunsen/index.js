72875bec8590902952b06a58f80f3b0afa1ba75b
mhyndman <mhyndman@ciena.com>
2016-03-10 15:35:43 -0500

    one more

--- blueprints/ember-frost-bunsen/index.js
+++ blueprints/ember-frost-bunsen/index.js
@@ -8,7 +8,8 @@ module.exports = {
             {name: 'ember-frost-text', target: '^1.2.0'},
             {name: 'ember-frost-button', target: '^1.4.0'},
             {name: 'ember-frost-checkbox', target: '^1.1.1'},
-            {name: 'ember-frost-icons', target: '^1.4.0'}
+            {name: 'ember-frost-icons', target: '^1.4.0'},
+            {name: 'ember-frost-select', target: '^1.3.1'}
           ]
         })
       })
