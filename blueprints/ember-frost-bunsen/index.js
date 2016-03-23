1f3b0218f702ece5dc6b14433e18593056ac6ed4
mhyndman <mhyndman@ciena.com>
2016-03-22 20:03:49 -0400

    frost-button upversion

--- blueprints/ember-frost-bunsen/index.js
+++ blueprints/ember-frost-bunsen/index.js
@@ -6,7 +6,7 @@ module.exports = {
           packages: [
             {name: 'ember-frost-theme', target: '^1.2.0'},
             {name: 'ember-frost-text', target: '^1.2.0'},
-            {name: 'ember-frost-button', target: '^1.4.0'},
+            {name: 'ember-frost-button', target: '^2.0.0'},
             {name: 'ember-frost-checkbox', target: '^1.1.1'},
             {name: 'ember-frost-icons', target: '^1.4.0'},
             {name: 'ember-frost-select', target: '^1.3.1'}
