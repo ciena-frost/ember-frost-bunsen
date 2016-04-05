563146fea3e21343fb74f75a9d81233ba4925976
Peter Banka <pbanka@ciena.com>
2016-04-04 23:47:25 -0700

    Adding reducer

--- addon/reducer.js
+++ addon/reducer.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 const INITIAL_VALUE = {
   validationResult: {errors: []},
   value: {}
@@ -5,5 +7,15 @@ const INITIAL_VALUE = {
 
 export default function (state, action) {
   console.log('calling reducer for action: ', action.type)
-  return INITIAL_VALUE
+  switch (action.type) {
+    case 'update':
+      const newState = _.cloneDeep(state)
+      _.set(newState, action.bunsenId, action.value)
+      return newState
+    case '@@redux/INIT':
+      return INITIAL_VALUE
+    default:
+      console.error(`Do not recognize action ${action.type}`)
+  }
+  return state
 }
