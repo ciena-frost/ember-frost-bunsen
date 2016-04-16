3d4350d0fd72984f573325988874d8fca62e6e2e
agonzalez <Andres.Gonzalez@cyaninc.com>
2016-04-15 17:59:22 -0700

    Defaults for simple most cases of json schema.

---
+++ tests/unit/actions-test.js
@@ -0,0 +1,195 @@
+const expect = chai.expect
+import {describe, it} from 'mocha'
+import {changeValue, updateValidationResults, validate, CHANGE_VALUE, VALIDATION_RESOLVED} from 'ember-frost-bunsen/actions'
+
+
+describe('changeValue action', function () {
+  it(`returns a dispatcher action with type "${CHANGE_VALUE}"`, function () {
+    const action = changeValue('some.path', 'value')
+
+    expect(action).to.eql({
+      type: CHANGE_VALUE,
+      bunsenId: 'some.path',
+      value: 'value'
+    })
+  })
+})
+
+describe('validate action', function () {
+  const SCHEMA_WITH_DEFAULTS = {
+    type: 'object',
+    properties: {
+      firstName: {
+        type: 'string',
+        default: 'Bruce'
+      },
+      lastName: {
+        type: 'string',
+        default: 'Wayne'
+      },
+      alias: {
+        type: 'string',
+        title: 'Nickname',
+        default: 'Batman'
+      },
+      onlyChild: {
+        type: 'boolean',
+        default: true
+      },
+      age: {
+        type: 'number',
+        title: 'Age'
+      }
+    },
+    required: ['lastName']
+  }
+  const SCHEMA_WITH_OBJECT_DEFAULTS = {
+    type: 'object',
+    properties: {
+      firstName: {
+        type: 'string',
+        default: 'Bruce'
+      },
+      lastName: {
+        type: 'string',
+        default: 'Wayne'
+      },
+      alias: {
+        type: 'string',
+        title: 'Nickname',
+        default: 'Batman'
+      },
+      onlyChild: {
+        type: 'boolean',
+        default: true
+      },
+      age: {
+        type: 'number',
+        title: 'Age'
+      }
+    },
+    required: ['lastName'],
+    default: {
+      firstName: 'Clark',
+      lastName: 'Kent',
+      alias: 'Superman'
+    }
+  }
+  const SCHEMA_WITH_DEEP_DEFAULTS = {
+    type: 'object',
+    properties: {
+      someotherProp: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'object',
+            properties: {
+              firstName: {
+                type: 'string',
+                default: 'Bruce'
+              },
+              lastName: {
+                type: 'string',
+                default: 'Wayne'
+              }
+            }
+          }
+        }
+      },
+      alias: {
+        type: 'string',
+        title: 'Nickname',
+        default: 'Batman'
+      }
+    },
+    required: ['alias']
+  }
+
+  const SCHEMA_WITH_REFS = {
+    type: 'object',
+    properties: {
+      hero: {
+        type: 'object',
+        '$ref': '#/definitions/superhero'
+      }
+    },
+    required: ['lastName'],
+    definitions: {
+      superhero: {
+        firstName: {
+          type: 'string',
+          default: 'Bruce'
+        },
+        lastName: {
+          type: 'string',
+          default: 'Wayne'
+        },
+        alias: {
+          type: 'string',
+          title: 'Nickname',
+          default: 'Batman'
+        }
+      }
+    }
+  }
+  function getDefaultValue (path, initialValue, schema) {
+    const thunk = validate(path, initialValue, schema, [])
+    const defaultValue = {}
+
+    thunk(function (action) {
+      _.assign(defaultValue, action.value)
+    }, function () { return {} })
+
+    return defaultValue
+  }
+  it('fills in defaults', function () {
+    const defaultValue = getDefaultValue(null, {}, SCHEMA_WITH_DEFAULTS)
+    expect(defaultValue).to.eql({
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      alias: 'Batman',
+      onlyChild: true
+    })
+  })
+
+  it('fills in defaults for specific values', function () {
+    const defaultValue = getDefaultValue('someotherProp.name', {}, SCHEMA_WITH_DEEP_DEFAULTS)
+
+    expect(defaultValue).to.eql({
+      firstName: 'Bruce',
+      lastName: 'Wayne'
+    })
+  })
+
+  it('handles defaults in deep objects', function () {
+    const defaultValue = getDefaultValue(null, {}, SCHEMA_WITH_DEEP_DEFAULTS)
+    expect(defaultValue).to.eql({
+      someotherProp: {
+        name: {
+          firstName: 'Bruce',
+          lastName: 'Wayne'
+        }
+      },
+      alias: 'Batman'
+    })
+  })
+
+  it('handles defaults for objects', function () {
+    const defaultValue = getDefaultValue(null, {}, SCHEMA_WITH_OBJECT_DEFAULTS)
+    expect(defaultValue).to.eql({
+      firstName: 'Clark',
+      lastName: 'Kent',
+      alias: 'Superman',
+      onlyChild: true
+    })
+  })
+
+  it('handles defaults in refs', function () {
+    const defaultValue = getDefaultValue(null, {}, SCHEMA_WITH_REFS)
+    expect(defaultValue).to.eql({
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      alias: 'Batman'
+    })
+  })
+})
