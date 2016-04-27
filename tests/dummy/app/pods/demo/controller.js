e51692f0e0bc668cc8fbb91841f9ae1419693ccd
Matthew Dahl <mdahl@ciena.com>
2016-04-27 06:35:14 -0700

    Cleanup demo menu

--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -5,7 +5,17 @@ export default Controller.extend({
   links: [
     {
       route: 'demo.components',
-      title: 'Components'
+      title: 'Components',
+      links: [
+        {
+          route: 'demo.detail',
+          title: 'frost-bunsen-detail'
+        },
+        {
+          route: 'demo.form',
+          title: 'frost-bunsen-form'
+        }
+      ]
     },
     {
       route: 'demo.examples',
