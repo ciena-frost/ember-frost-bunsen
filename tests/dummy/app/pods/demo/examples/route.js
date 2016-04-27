cec761c9ae0138dbc443e4153742f4d6e8df8390
Matthew Dahl <mdahl@ciena.com>
2016-04-27 06:22:14 -0700

    Bring back examples

--- tests/dummy/app/pods/demo/examples/route.js
+++ tests/dummy/app/pods/demo/examples/route.js
@@ -2,4 +2,20 @@ import Ember from 'ember'
 const {Route} = Ember
 
 export default Route.extend({
+  model () {
+    /* eslint-disable new-cap */
+    return Promise.all([
+      this.store.findAll('model'),
+      this.store.findAll('value'),
+      this.store.findAll('view')
+    ])
+      .then(([models, values, views]) => {
+        return {
+          models,
+          values,
+          views
+        }
+      })
+    /* eslint-enable new-cap */
+  }
 })
