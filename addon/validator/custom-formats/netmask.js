8fa4a46326840c29bbe05cfeb68e2485c5b5c9d1
Matthew Dahl <mdahl@ciena.com>
2016-06-28 08:19:39 -0700

    Add netmask format

--- addon/validator/custom-formats/netmask.js
+++ addon/validator/custom-formats/netmask.js
@@ -1,3 +1,31 @@
-export default function () {
-  return true // TODO: implement
+/**
+ * @reference https://en.wikipedia.org/wiki/Subnetwork
+ */
+
+import ipv4Address from './ipv4-address'
+
+/**
+ * Convert decimal value to binary representation
+ * @param {Number} decimal - decimal value to convert to binary
+ * @returns {String} string containing binary representation
+ */
+export function decimalToBinary (decimal) {
+  return (decimal >>> 0).toString(2)
+}
+
+/**
+ * Validate value as a netmask
+ * @param {Any} value - value to validate
+ * @returns {Boolean} whether or not value is valid
+ */
+export default function (value) {
+  if (!ipv4Address(value)) {
+    return false
+  }
+
+  const binary = value.split('.')
+    .map(decimalToBinary)
+    .join('')
+
+  return /^1*0*$/.test(binary)
 }
---
+++ tests/dummy/app/pods/demo/formats/documentation/netmask.md
@@ -0,0 +1,3 @@
+This format validates that the value is a string representation of a netmask.
+
+[Learn more…](https://en.wikipedia.org/wiki/Subnetwork)
