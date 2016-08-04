1f8d043822583710de9fae9d655a56f36c66dec3
Roxanne Panthaky <rpanthak@ciena.com>
2016-08-04 16:54:02 -0400

    Revert "incorrect call to this.app.import(). causing issues in consuming app"
    This reverts commit d41144c032dd2bebe05435251b583ed2631da025.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
 
   included: function (app) {
     this._super.included(app)
-    app.import('bower_components/z-schema/dist/ZSchema-browser.js')
+    this.app.import('bower_components/z-schema/dist/ZSchema-browser.js')
   },
 
   init: function (app) {
