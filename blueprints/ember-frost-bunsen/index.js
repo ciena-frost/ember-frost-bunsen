04e5335d793a37e78a98608424063c8cdea2e81a
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-10 16:29:51 -0700

    Update deps

--- blueprints/ember-frost-bunsen/index.js
+++ blueprints/ember-frost-bunsen/index.js
@@ -8,11 +8,11 @@ module.exports = {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-browserify', target: '^1.1.11'},
-            {name: 'ember-bunsen-core', target: '>=0.6.23 <=2.0.0'},
-            {name: 'ember-frost-core', target: '>=0.22.0 <2.0.0'},
+            {name: 'ember-bunsen-core', target: '>=0.6.25 <=2.0.0'},
+            {name: 'ember-frost-core', target: '>=0.22.1 <2.0.0'},
             {name: 'ember-frost-fields', target: '>=0.2.0 <2.0.0'},
             {name: 'ember-frost-tabs', target: '^2.0.2'},
-            {name: 'ember-lodash-shim', target: '>=0.1.11 <2.0.0'},
+            {name: 'ember-lodash-shim', target: '>=0.1.12 <2.0.0'},
             {name: 'ember-prop-types', target: '^2.2.3'},
             {name: 'ember-redux', target: '^1.0.0'},
             {name: 'ember-sortable', target: '^1.8.1'}
