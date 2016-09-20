38a5bade3915eb66e215bb1159ed4e9683858bf2
Matthew Dahl <dahl.matthew@gmail.com>
2016-09-20 08:36:38 -0700

    Attempt to add test helper

---
+++ test-support/helpers/ember-frost-bunsen.js
@@ -0,0 +1,18 @@
+/* global fillIn */
+
+import {$hook} from 'ember-hook'
+
+/**
+ * Fill in bunsen input with value
+ * @param {String} bunsenId - identifier of bunsen property to fill in
+ * @param {Any} value - value to set input to
+ * @param {String} [hook='bunsenForm'] - hook passed into bunsen form instance
+ * @returns {RSVP.Promise} promise that resolves when all async behavior completes
+ */
+export function fillInBunsenInput (bunsenId, value, hook = 'bunsenForm') {
+  return fillIn($hook(`${hook}-${bunsenId}`), value)
+}
+
+export default {
+  fillInBunsenInput
+}
