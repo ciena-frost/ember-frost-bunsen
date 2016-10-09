757557f3ec85eeb13e304abc5834d6a888f23c1c
John Boutilier <jboutili@ciena.com>
2016-10-08 21:41:52 -0400

    Address PR comments

--- addon/components/inputs/checkbox-array.js
+++ addon/components/inputs/checkbox-array.js
@@ -1,6 +1,9 @@
+import _ from 'lodash'
 import AbstractInput from './abstract-input'
 import computed, {readOnly} from 'ember-computed-decorators'
 import layout from 'ember-frost-bunsen/templates/components/frost-bunsen-input-checkbox-array'
+import Ember from 'ember'
+const {get} = Ember
 
 export default AbstractInput.extend({
   // == Component Properties ===================================================
@@ -17,20 +20,25 @@ export default AbstractInput.extend({
   selected: [],
 
   // == Computed Properties ====================================================
+  @readOnly
+  @computed('cellConfig')
+  size (cellConfig) {
+    return _.get(cellConfig, 'renderer.size') || 'medium'
+  },
 
   @readOnly
   @computed('bunsenModel')
   options (bunsenModel) {
-    return bunsenModel.items.enum.map((key) => {
-      return {
-        value: key
-      }
+    const items = get(bunsenModel, 'items.enum') || []
+
+    return items.map((value) => {
+      return { value }
     })
   },
 
   // == Functions ==============================================================
   /**
-   * This should be overriden by inherited inputs to convert the value to the appropriate format
+   * Convert value to appropriate format for template
    * @param {Boolean|String} data - value to parse
    * @returns {any} parsed value
    */
