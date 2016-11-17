df26b67bf155769379e320da5d42d0715f60e0e6
Matthew Dahl <dahl.matthew@gmail.com>
2016-11-17 13:29:53 -0800

    Update link.js

--- addon/components/inputs/link.js
+++ addon/components/inputs/link.js
@@ -90,9 +90,6 @@ export default AbstractInput.extend({
     const newDefaultLabel = getAttr(newAttrs, 'cellConfig.renderer.defaultLabel')
     const newLabel = getOption(newAttrs, 'label', formValue, newDefaultLabel)
     const newUrl = getOption(newAttrs, 'url', formValue)
-    const oldDefaultLabel = getAttr(oldAttrs, 'cellConfig.renderer.defaultLabel')
-    const oldLabel = getOption(oldAttrs, 'label', formValue, oldDefaultLabel)
-    const oldUrl = getOption(oldAttrs, 'url', formValue)
     const newLinkLabel = newLabel || 'Link'
 
     if (this.get('linkLabel') !== newLinkLabel) {
