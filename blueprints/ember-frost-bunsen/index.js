bfa96cc20e2f646d01386853173ceca1473d2ff1
Sophy Pal <spal@ciena.com>
2016-11-29 19:54:41 -0800

    Update bunsen-core

--- blueprints/ember-frost-bunsen/index.js
+++ blueprints/ember-frost-bunsen/index.js
@@ -8,7 +8,7 @@ module.exports = {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-browserify', target: '^1.1.12'},
-            {name: 'ember-bunsen-core', target: '0.12.1'},
+            {name: 'ember-bunsen-core', target: '0.12.3'},
             {name: 'ember-frost-core', target: '0.30.0'},
             {name: 'ember-frost-fields', target: '^1.0.0'},
             {name: 'ember-frost-tabs', target: '^3.1.0'},
