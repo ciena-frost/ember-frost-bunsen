bbdb30c5f7f1dafc2b0fab9afff685a02db0cd81
Matthew Dahl <dahl.matthew@gmail.com>
2016-12-19 10:39:18 -0800

    Update blueprints

--- blueprints/ember-frost-bunsen/index.js
+++ blueprints/ember-frost-bunsen/index.js
@@ -3,8 +3,7 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         {name: 'ember-ajax', target: '^2.5.2'},
-        {name: 'ember-browserify', target: '^1.1.12'},
-        {name: 'ember-bunsen-core', target: '0.15.0'},
+        {name: 'ember-bunsen-core', target: '0.15.1'},
         {name: 'ember-frost-core', target: '^1.1.3'},
         {name: 'ember-frost-fields', target: '^4.0.0'},
         {name: 'ember-frost-popover', target: '^4.0.1'},
@@ -12,9 +11,9 @@ module.exports = {
         {name: 'ember-getowner-polyfill', target: '^1.0.1'},
         {name: 'ember-lodash-shim', target: '^1.0.0'},
         {name: 'ember-prop-types', target: '^3.0.2'},
-        {name: 'ember-redux-shim', target: '0.0.1'},
+        {name: 'ember-redux-shim', target: '0.0.2'},
         {name: 'ember-redux-thunk', target: '0.0.1'},
-        {name: 'ember-spread', target: '0.0.7'},
+        {name: 'ember-spread', target: '^1.0.0'},
         {name: 'ember-sortable', target: '^1.8.1'}
       ]
     })
