8a377db0d384f1fb8c6b0174e93e848e49bf4ddd
Zarif Shahriar <zshahria@ciena.com>
2017-02-03 17:01:20 -0500

    Updated code datetime renderer to hold data correctly.

--- addon/components/inputs/datetime.js
+++ addon/components/inputs/datetime.js
@@ -2,6 +2,7 @@ import AbstractInput from './abstract-input'
 import layout from 'ember-frost-bunsen/templates/components/frost-bunsen-input-datetime'
 
 export default AbstractInput.extend({
+
   // == Component Properties ===================================================
 
   classNameBindings: ['value:frost-bunsen-has-value'],
@@ -13,12 +14,40 @@ export default AbstractInput.extend({
 
   layout,
 
+  // == State Properties =======================================================
+
+  getDefaultProps () {
+    if (this.get('value')) {
+      var arr = this.get('value').split('T')
+
+      return {
+        date: arr[0] || '',
+        time: arr[1] || ''
+      }
+    }
+
+    return {
+      date: '',
+      time: ''
+    }
+  },
+
   // == Computed Properties ====================================================
 
   // == Functions ==============================================================
 
+  parseValue (value) {
+    if (value.includes('-')) {
+      this.set('date', value)
+    } else {
+      this.set('time', 'T' + value)
+    }
+    return this.get('date') + this.get('time')
+  },
+
   // == Actions ===============================================================
 
   actions: {
+
   }
 })
