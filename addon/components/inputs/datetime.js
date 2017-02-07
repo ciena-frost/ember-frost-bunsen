c67d36cae3ab8acd496f6cee66e691c45780f76f
Zarif Shahriar <zshahria@ciena.com>
2017-02-07 17:32:07 -0500

    Added computed properties. Allows for programmed initial date and value.

--- addon/components/inputs/datetime.js
+++ addon/components/inputs/datetime.js
@@ -1,6 +1,7 @@
 import AbstractInput from './abstract-input'
 import layout from 'ember-frost-bunsen/templates/components/frost-bunsen-input-datetime'
 import Ember from 'ember'
+import computed, {readOnly} from 'ember-computed-decorators'
 import { Format, setTime, validateTime } from 'ember-frost-date-picker'
 import moment from 'moment'
 
@@ -17,37 +18,36 @@ export default AbstractInput.extend({
 
     layout,
 
-    dateTimeValue: moment().subtract(1, 'day').subtract(1, 'hour').format(Format.dateTime),
-    dateTimeValueInvalid: false,
+    // == Computed Properties ====================================================
+
+    /**
+    * Handles change in value. If there is a value set by the form it will take that value, else it will display a sample date. 
+    */
+    @computed('value')
+    dateTimeValue(value){
+        if(value!=undefined){
+            return moment(value).format(Format.dateTime)
+        }
+        return moment().subtract(1, 'day').subtract(1, 'hour').format(Format.dateTime)  
+    },
+
+     @computed('value')
+     dateTimeValueInvalid () {
+        return false
+     },
 
     // == Actions ===============================================================
 
     actions: {
 
         onDateTimeChange(dateTimeValue) {
-
             const momentDateTimeValue = moment(dateTimeValue)
             if (!momentDateTimeValue.isValid()) {
                 this.set('dateTimeValueInvalid', true)
                 return 
             }
-
-            //Comment this  else statment out if using with validation. 
-            else {
-                this.send('handleChange', dateTimeValue)
-            }
-
-            // Example just to show validation - any date after 2015-01-01T10:00:00-00:00 is valid. Uncomment to use.
-            // const dateTimeValueInvalid = momentDateTimeValue.isBefore(moment('2015-01-01T10:00:00-00:00'))
-            // this.setProperties({
-            //     dateTimeValue,
-            //     dateTimeValueInvalid
-            // })
-            // if (!dateTimeValueInvalid) {
-            //     this.send('handleChange', dateTimeValue)
-            // }
+            this.send('handleChange', dateTimeValue)
         },
-
     }
 
 })
