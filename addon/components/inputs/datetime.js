a8665c1c18a8df565aca7794326d4b2afb6b7f7f
Zarif Shahriar <zshahria@ciena.com>
2017-02-07 12:55:55 -0500

    Changed the renderer to use the single date-time-picker. Updated to version 7.0.0 of date-picker.

--- addon/components/inputs/datetime.js
+++ addon/components/inputs/datetime.js
@@ -1,53 +1,55 @@
 import AbstractInput from './abstract-input'
 import layout from 'ember-frost-bunsen/templates/components/frost-bunsen-input-datetime'
+import Ember from 'ember'
+import { Format, setTime, validateTime } from 'ember-frost-date-picker'
+import moment from 'moment'
 
 export default AbstractInput.extend({
 
-  // == Component Properties ===================================================
+    // == Component Properties ===================================================
 
-  classNameBindings: ['value:frost-bunsen-has-value'],
+    classNameBindings: ['value:frost-bunsen-has-value'],
 
-  classNames: [
-    'frost-bunsen-input-datetime',
-    'frost-field'
-  ],
+    classNames: [
+        'frost-bunsen-input-datetime',
+        'frost-field'
+    ],
 
-  layout,
+    layout,
 
-  // == State Properties =======================================================
+    dateTimeValue: moment().subtract(1, 'day').subtract(1, 'hour').format(Format.dateTime),
+    dateTimeValueInvalid: false,
 
-  getDefaultProps () {
-    if (this.get('value')) {
-      var arr = this.get('value').split('T')
+    // == Actions ===============================================================
 
-      return {
-        date: arr[0] || '',
-        time: arr[1] || ''
-      }
+    actions: {
+
+        onDateTimeChange(dateTimeValue) {
+
+            const momentDateTimeValue = moment(dateTimeValue)
+            if (!momentDateTimeValue.isValid()) {
+                this.set('dateTimeValueInvalid', true)
+                return 
+            }
+
+            //Comment this  else statment out if using with validation. 
+            else {
+                this.send('handleChange', dateTimeValue)
+            }
+
+            // Example just to show validation - any date after 2015-01-01T10:00:00-00:00 is valid. Uncomment to use.
+            // const dateTimeValueInvalid = momentDateTimeValue.isBefore(moment('2015-01-01T10:00:00-00:00'))
+            // this.setProperties({
+            //     dateTimeValue,
+            //     dateTimeValueInvalid
+            // })
+            // if (!dateTimeValueInvalid) {
+            //     this.send('handleChange', dateTimeValue)
+            // }
+        },
+
     }
 
-    return {
-      date: '',
-      time: ''
-    }
-  },
-
-  // == Computed Properties ====================================================
-
-  // == Functions ==============================================================
-
-  parseValue (value) {
-    if (value.includes('-')) {
-      this.set('date', value)
-    } else {
-      this.set('time', 'T' + value)
-    }
-    return this.get('date') + this.get('time')
-  },
-
-  // == Actions ===============================================================
-
-  actions: {
-
-  }
 })
+
+
