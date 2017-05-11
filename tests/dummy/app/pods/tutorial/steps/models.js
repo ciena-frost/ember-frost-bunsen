34ebe8b42b48970ca4a5fdb86c275629bcfaada0
Matthew Dahl <mdahl@ciena.com>
2017-05-11 08:21:51 -0700

    Add basic model docs and upgrade ember-bunsen-core

--- tests/dummy/app/pods/tutorial/steps/models.js
+++ tests/dummy/app/pods/tutorial/steps/models.js
@@ -1,7 +1,63 @@
 export default {
   component: 'markdown-step',
   props: {
-    markdown: 'content goes here'
+    markdown: `
+A bunsen model defines the JSON structure of the form value which often aligns with the structure for an API request.
+This model is essentially [JSON schema](http://json-schema.org/) with some of our own additions.
+
+> Note: we also omit a few features such as defining multiple types for a single property.
+
+To get started you need this base template for your model:
+
+\`\`\`json
+{
+  "properties": {
+  },
+  "type": "object"
+}
+\`\`\`
+
+This tells bunsen our form value is an object. Next we'll actually want to define top level properties for our
+form which can be any of the following types: **array**, **boolean**, **integer**, **number**, **object**, **string**.
+
+Lets go ahead and start with a simple example of adding a string property named **foo**:
+
+\`\`\`json
+{
+  "properties": {
+    "foo": {
+      "type": "string"
+    }
+  },
+  "type": "object"
+}
+\`\`\`
+
+Assuming the **foo** input was given the value **bar** our form value will look like:
+
+\`\`\`json
+{
+  "foo": "bar"
+}
+\`\`\`
+
+Next up we might want to make the **foo** input required which would yield the following model:
+
+\`\`\`json
+{
+  "properties": {
+    "foo": {
+      "type": "string"
+    }
+  },
+  "required": ["foo"],
+  "type": "object"
+}
+\`\`\`
+
+This covers the basics of a bunsen model and how it works, if you want to know more read up
+on [JSON Schema](https://spacetelescope.github.io/understanding-json-schema/index.html).
+    `
   },
   slug: 'models',
   title: 'Models'
