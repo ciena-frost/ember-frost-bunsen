700ee7faa47b46d59c89b73a367bd083e70d9fdf
Sophy Pal <spal@ciena.com>
2017-06-23 11:08:44 -0700

    Fix previous datetime pr issue

--- addon/components/inputs/datetime.js
+++ addon/components/inputs/datetime.js
@@ -48,7 +48,7 @@ export default AbstractInput.extend({
   // == Functions ==============================================================
 
   parseValue (value) {
-    return moment(value).format(DATE_FORMAT)
+    return moment(value).format(DATE_TIME_FORMAT)
   },
 
   // == Actions ===============================================================
