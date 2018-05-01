320c614dd9f39408857c411310450148d4fac06e
Heittola <cheittol@ciena.com>
2018-05-01 11:29:46 -0400

    removed the reporter from testem

--- testem.js
+++ testem.js
@@ -1,5 +1,4 @@
 /* eslint-env node */
-// const Reporter = require('ember-test-utils/reporter')
 
 module.exports = {
   disable_watching: true,
@@ -11,6 +10,5 @@ module.exports = {
   launch_in_dev: [
     'Chrome'
   ],
-  // reporter: new Reporter(),
   test_page: 'tests/index.html?hidepassed'
 }
