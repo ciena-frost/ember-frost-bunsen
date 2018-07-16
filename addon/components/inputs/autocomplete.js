9b057365230ecc7094a375f704b565783d2471ba
Eric White <ewhite@ciena.com>
2018-07-16 11:39:27 -0400

    loading autocomplete

--- addon/components/inputs/autocomplete.js
+++ addon/components/inputs/autocomplete.js
@@ -1,5 +1,8 @@
 import SelectInput from './select'
 import layout from 'ember-frost-bunsen/templates/components/frost-bunsen-input-autocomplete'
+import Ember from 'ember'
+const {isEmpty} = Ember
+import computed, {readOnly} from 'ember-computed-decorators'
 
 export default SelectInput.extend({
   // == Component Properties ===================================================
@@ -12,10 +15,20 @@ export default SelectInput.extend({
 
   getDefaultProps () {
     return {
-      ignoreEmptyFilterSearch: true
+      ignoreEmptyFilterSearch: true,
+      _emptyFilter: true,
+      filter: ''
     }
   },
-
+  @readOnly
+  @computed('bunsenModel.{modelType,endpoint}', 'updateItems.isRunning', '_emptyFilter')
+  asyncLoading (modelType, endpoint, isUpdateItemsRunning, emptyFilter) {
+    const isAsyncGet = modelType || endpoint
+    if (isAsyncGet) {
+      return emptyFilter || isUpdateItemsRunning
+    }
+    return false
+  },
   /**
    * This should be overriden by inherited inputs to convert the value to the appropriate format
    * @param {String} data - value to parse
@@ -23,5 +36,11 @@ export default SelectInput.extend({
    */
   parseValue (data) {
     return data
+  },
+  actions: {
+    checkIfEmptyfilter (filterValue) {
+      this.set('_emptyFilter', isEmpty(filterValue))
+      this.send('filterOptions', filterValue)
+    }
   }
 })
